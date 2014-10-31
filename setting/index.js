73ca4696d111bc9422cfd9c38612750ac04f939e
echodittomiles <miles@echoditto.com>
2014-10-31 16:04:43 -0400

    prettification and else if separation

--- setting/index.js
+++ setting/index.js
@@ -1,10 +1,10 @@
 'use strict';
 var util = require('util'),
-    yeoman = require('yeoman-generator'),
-    fs = require('fs'),
-    _ = require('lodash'),
-    _s = require('underscore.string'),
-    pluralize = require('pluralize');
+  yeoman = require('yeoman-generator'),
+  fs = require('fs'),
+  _ = require('lodash'),
+  _s = require('underscore.string'),
+  pluralize = require('pluralize');
 
 var SettingGroupGenerator = module.exports = function SettingGroupGenerator(args, options, config) {
   // By calling `NamedBase` here, we get the argument to the subgenerator call
@@ -13,7 +13,7 @@ var SettingGroupGenerator = module.exports = function SettingGroupGenerator(args
 
   console.log('You called the setting subgenerator with the argument ' + this.name + '.');
 
-  fs.readFile('generator.json', 'utf8', function (err, data) {
+  fs.readFile('generator.json', 'utf8', function(err, data) {
     if (err) {
       console.log('Error: ' + err);
       return;
@@ -34,93 +34,91 @@ SettingGroupGenerator.prototype.askFor = function askFor() {
     name: 'settingProper',
     message: 'What is the proper name of the setting?',
     default: 'Setting'
-  },
-  {
+  }, {
     type: 'input',
     name: 'settingDesc',
     message: 'What is the description of the setting?',
     default: 'Description of setting'
-  },
-  {
+  }, {
     type: 'input',
     name: 'settingName',
     message: 'What is the (JavaScript) machine name of the setting?',
     default: 'aSetting'
-  },
-  {
+  }, {
     type: 'input',
     name: 'settingFunctionName',
     message: 'What should the snake_case function name be for this setting?',
     default: 'a_setting'
-  },
-  {
+  }, {
     type: 'list',
     name: 'settingType',
     message: 'What is the type of the attribute?',
     choices: ['String', 'Number', 'Boolean'],
     default: 'String'
-  },
-  {
+  }, {
     type: 'input',
     name: 'settingDefault',
     message: 'What is the default value?',
-  },
-  {
-    when: function (props) { return (/String/).test(props.settingType); },
+  }, {
+    when: function(props) {
+      return (/String/).test(props.settingType);
+    },
     type: 'input',
     name: 'minLength',
     message: 'Enter the minimum length for the String setting, or hit enter:',
-    validate: function (input) {
+    validate: function(input) {
       if (input && isNaN(input)) {
         return "Please enter a number.";
       }
       return true;
     }
-  },
-  {
-    when: function (props) { return (/String/).test(props.settingType); },
+  }, {
+    when: function(props) {
+      return (/String/).test(props.settingType);
+    },
     type: 'input',
     name: 'maxLength',
     message: 'Enter the maximum length for the String setting, or hit enter:',
-    validate: function (input) {
+    validate: function(input) {
       if (input && isNaN(input)) {
         return "Please enter a number.";
       }
       return true;
     }
-  },
-  {
-    when: function (props) { return (/Number/).test(props.settingType); },
+  }, {
+    when: function(props) {
+      return (/Number/).test(props.settingType);
+    },
     type: 'input',
     name: 'min',
     message: 'Enter the minimum value for the numeric setting, or hit enter:',
-    validate: function (input) {
+    validate: function(input) {
       if (input && isNaN(input)) {
         return "Please enter a number.";
       }
       return true;
     }
-  },
-  {
-    when: function (props) { return (/Number/).test(props.settingType); },
+  }, {
+    when: function(props) {
+      return (/Number/).test(props.settingType);
+    },
     type: 'input',
     name: 'max',
     message: 'Enter the maximum value for the numeric setting, or hit enter:',
-    validate: function (input) {
+    validate: function(input) {
       if (input && isNaN(input)) {
         return "Please enter a number.";
       }
       return true;
     }
-  },
-  {
+  }, {
     type: 'confirm',
     name: 'again',
     message: 'Would you like to enter another attribute or reenter a previous attribute?',
     default: true
   }];
 
-  this.prompt(prompts, function (props) {
+  this.prompt(prompts, function(props) {
     this.settings = this.settings || [];
     var settingType = props.settingType;
     var settingImplType = props.settingType;
@@ -131,7 +129,9 @@ SettingGroupGenerator.prototype.askFor = function askFor() {
     } else if (settingType === 'Boolean') {
       settingImplType = 'boolean';
     }
-    this.settings = _.reject(this.settings, function (setting) { return setting.settingName === props.settingName; });
+    this.settings = _.reject(this.settings, function(setting) {
+      return setting.settingName === props.settingName;
+    });
     this.settings.push({
       settingProper: props.settingProper,
       settingDesc: props.settingDesc,
@@ -158,8 +158,13 @@ SettingGroupGenerator.prototype.files = function files() {
 
   this.moduleName = this.name;
   this.groups = this.generatorConfig.groups;
-  this.groups = _.reject(this.groups, function (group) { return group.name === this.name; }.bind(this));
-  this.groups.push({ name: this.name, settings: this.settings});
+  this.groups = _.reject(this.groups, function(group) {
+    return group.name === this.name;
+  }.bind(this));
+  this.groups.push({
+    name: this.name,
+    settings: this.settings
+  });
   this.pluralize = pluralize;
   this.generatorConfig.groups = this.groups;
   this.generatorConfigStr = JSON.stringify(this.generatorConfig, null, '\t');
